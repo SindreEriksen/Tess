45a8f137a3df488d9e5968ff9ffd79995fb89485
EliseJohnsen <elise.johnsen@gmail.com>
2018-10-19 13:45:06 +0200

    Har lagt til MapsActivity. Åpner kart via toolbar. Har ikke lagt til andre markører enn Hønefoss på kartet.

--- app/src/main/java/com/hfad/tess/MainActivity.java
+++ app/src/main/java/com/hfad/tess/MainActivity.java
@@ -1,11 +1,15 @@
 package com.hfad.tess;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -20,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         ListView aktivtetsliste = findViewById(R.id.aktivtetsliste);
 
@@ -38,6 +45,22 @@ public class MainActivity extends AppCompatActivity {
             dbToast.show();
         }
     } // end onCreate()
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item ) {
+        switch (item.getItemId()) {
+            case R.id.action_se_kart:
+                Intent intent = new Intent(this, MapsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     public void onDestroy() {
         super.onDestroy();
