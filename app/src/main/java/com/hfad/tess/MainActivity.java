bd8603875df703a715af466682a157c423401008
thomas-93 <thomas.kristoffersen93@gmail.com>
2018-10-31 14:33:22 +0100

    hahaha

--- app/src/main/java/com/hfad/tess/MainActivity.java
+++ app/src/main/java/com/hfad/tess/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private SQLiteDatabase db;
     private Cursor cursor;
-    private String sindre;
+    private String thomas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
