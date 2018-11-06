24d3b9d2c16bbf8de03be45912e49bf8fa3542c8
EliseJohnsen <elise.johnsen@gmail.com>
2018-11-06 14:47:04 +0100

    Fått til cardview sammen med recyclerview.

--- app/src/main/java/com/hfad/tess/RecyclerViewAdapter.java
+++ app/src/main/java/com/hfad/tess/RecyclerViewAdapter.java
@@ -6,11 +6,13 @@ package com.hfad.tess;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -48,10 +50,11 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int position) {
         Log.d(TAG, "onBindViewHolder: called");
+
         Glide.with(mContext)
                 .asBitmap()
                 .load(mImages.get(position))
-                .into(viewHolder.image);
+                .into(viewHolder.header_image);
 
         viewHolder.txt_name.setText(mNames.get(position));
         viewHolder.txt_description.setText(mDescriptions.get(position));
@@ -70,14 +73,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        CircleImageView image;
+        ImageView header_image;
         TextView txt_name;
         TextView txt_description;
-        RelativeLayout parent_layout;
+        ConstraintLayout parent_layout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            image = itemView.findViewById(R.id.image);
+            header_image = itemView.findViewById(R.id.header_image);
             txt_name = itemView.findViewById(R.id.txt_name);
             txt_description = itemView.findViewById(R.id.txt_description);
             parent_layout = itemView.findViewById(R.id.parent_layout);
